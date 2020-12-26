29cf3946f588c58d1f63e5988726fc0096d2428f
Hackerry <harrynjc@yahoo.com>
2020-12-25 16:19:22 -0800

    Init Commit

---
+++ src/test/base_test.go
@@ -0,0 +1,28 @@
+package test
+
+import (
+	"time"
+	"net/http"
+	"strconv"
+	"testing"
+
+	"server"
+)
+
+func TestBase(t *testing.T) {
+	t.Logf("Base test")
+}
+
+func TestHomePage(t *testing.T) {
+	go server.Serve()
+	time.Sleep(1 * time.Second)
+
+	// Connect to port and 
+	resp, err := http.Get("http://localhost:" + strconv.Itoa(server.Port))
+	if err != nil {
+		t.Errorf("GET failed: %s\n", err)
+	}
+	if resp.StatusCode == 200 {
+		t.Logf("GET successful\n")
+	}
+}
